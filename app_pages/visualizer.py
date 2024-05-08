30519179d547c70f18789714dd5a634560d61bff
Jaaz7 <jaazielvale@hotmail.com>
2024-05-08 14:07:00 +0200

    Add visualizer.py

---
+++ app_pages/visualizer.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+import itertools
+import random
+
+
+def visualizer_body():
+    st.write('### Dog Breed Identifier Visualizer')
+    st.info(
+        '* The client would like to have a study to identify '
+         ' different dog breeds based on images.)')
